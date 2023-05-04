61c6558813c98665b81ddb9e3915514decfc8fa0
klozz <klozz@vivaldi.net>
2023-05-04 05:45:29 +0000

    Add system for qssi-user 12 SKQ1.220502.001 eng.nubia.20221228.160340 release-keys
    Signed-off-by: klozz <klozz@vivaldi.net>

---
+++ system/system/bin/playbootsound.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+
+tinymix "TFA Profile" "speaker"
+tinymix "TFA_CHIP_SELECTOR" 0
+
+#disable
+exit
---
+++ system/system/bin/stopbootsound.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+
+tinymix "TFA Profile" "receiver"
+tinymix "TFA_CHIP_SELECTOR" 3
+
+#disable
+exit
